a60ef9bae53c99142aa0670b500478c4df5eb0cf
Francesco Polichetti <fpolichetti@student.tgm.ac.at>
2024-10-16 10:34:59 +0200

    [FEATURE 4] [TESTING] Persistenz im Worttrainer erfolgreich getestet

---
+++ src/test/java/fpolichetti/WortTrainerPersistenzTest.java
@@ -0,0 +1,46 @@
+package fpolichetti;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.File;
+
+public class WortTrainerPersistenzTest {
+
+    @Test
+    public void testSpeichereUndLadeDaten() {
+        WortTrainer trainer = new WortTrainer();
+        trainer.setPfad("test_worttrainer.json");
+
+        WortEintrag eintrag = new WortEintrag("Hund", "http://example.com/hund.jpg");
+        trainer.addEintrag(eintrag);
+        trainer.getStatistik().aktualisiere(true);
+
+        trainer.speichereDaten();
+
+        WortTrainer geladenerTrainer = new WortTrainer();
+        geladenerTrainer.setPfad("test_worttrainer.json");
+        geladenerTrainer.ladeDaten();
+
+        assertEquals(1, geladenerTrainer.getEintraege().size());
+        assertEquals("Hund", geladenerTrainer.getEintraege().get(0).getWort());
+        assertEquals(1, geladenerTrainer.getStatistik().getRichtigeVersuche());
+
+        File file = new File("test_worttrainer.json");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void testLoeschePersistenz() {
+        WortTrainer trainer = new WortTrainer();
+        trainer.setPfad("test_worttrainer.json");
+
+        trainer.speichereDaten();
+        File file = new File("test_worttrainer.json");
+        assertTrue(file.exists());
+
+        trainer.loeschePersistenz();
+        assertFalse(file.exists());
+    }
+}
